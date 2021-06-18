b561820712101a092efe359744c5b7fa4a147d4c
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-18 10:05:51 -0400

    added categories and pictures resources, created associations between pictures, categories, and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_101101) do
+ActiveRecord::Schema.define(version: 2021_06_18_140112) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
