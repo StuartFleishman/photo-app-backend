16d5416bbf30b7f9d1e2177cc5132241fcd0fde1
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-20 08:59:17 -0400

    added cloudinary gem for third party file upload to avoid fakePath

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,9 +6,9 @@ class PicturesController < ApplicationController
   end
 
   def create
-    picture = Picture.new(picture_params)
-    byebug
-    if user.save
+    image = Cloudinary::Uploader.upload(params[:image_url])
+    picture = Picture.new(image_url: image["url"], title: params["title"], category_id: params["category_id"], user_id: params["user_id"])
+    if picture.save
         render json: picture.to_json(except: [:created_at, :updated_at])
     else 
         render json: {error: "oops"}
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|    
+  config.cloud_name = "dtoznvgyl"   
+  config.api_key = "254837259813175"   
+  config.api_secret = "3XL-XEBx7hAFNgIcjTDecK4X_D0"   
+  config.secure = true    
+  config.cdn_subdomain = true  
+end
