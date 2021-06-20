2aa1b642155157418d9cf7c5c576dbb85c7b4a48
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-20 06:27:11 -0400

    added create action to pictures controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,4 +5,20 @@ class PicturesController < ApplicationController
     render json: pictures.to_json(except: [:created_at, :updated_at])
   end
 
+  def create
+    picture = Picture.new(picture_params)
+    byebug
+    if user.save
+        render json: picture.to_json(except: [:created_at, :updated_at])
+    else 
+        render json: {error: "oops"}
+    end 
+  end
+
+
+  private 
+  def picture_params
+    params.require(:picture).permit(:title, :image_url, :category_id, :user_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-        render json: users.to_json(except: [:created_at, :updated_at])
+        render json: user.to_json(except: [:created_at, :updated_at])
     else 
         render json: {error: "oops"}
     end 
